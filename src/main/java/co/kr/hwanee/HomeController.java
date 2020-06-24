e4dda4b5bf94e820eb7fe7331da5e5d68a3dfc2d
hwanee <hwaneehwanee@gmail.com>
2020-06-24 23:03:37 +0900

    ,

--- src/main/java/co/kr/hwanee/HomeController.java
+++ src/main/java/co/kr/hwanee/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		//이얼 ㅋㄴㅇㄹ
+		//이얼 ㅋㄴㅇㄹㄴㅇ
 		return "home";
 	}
 	
